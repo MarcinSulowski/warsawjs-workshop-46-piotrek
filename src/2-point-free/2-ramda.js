c754bba79c705fdb9a790d40f2f8c4cbd17ce0a6
Marcin Sulowski <mr.sulowski@gmail.com>
2020-07-19 12:07:04 +0200

    point free

--- src/2-point-free/2-ramda.js
+++ src/2-point-free/2-ramda.js
@@ -1,4 +1,10 @@
+const R = require('ramda')
 /**
  * Write the same function from exercise 1, but this time use Ramda.
  */
-export const getShortNamedAnimals = null
+
+const shorterThan = (val) => (elem) => elem < val
+
+export const getShortNamedAnimals = R.filter(
+  R.compose(R.lt(R.__, 5), R.prop('length'), R.prop('name'))
+)
